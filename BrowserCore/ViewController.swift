c8740a72b6e4041feef756070ae95d7431e899d1
Tim Palade <timotei@cliqz.com>
2018-01-05 10:13:56 +0100

    Initial Commit

---
+++ BrowserCore/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrowserCore
+//
+//  Created by Tim Palade on 1/5/18.
+//  Copyright © 2018 Tim Palade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
